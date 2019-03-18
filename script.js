3557fd93d0de03dd57e8fbeb021799bc78152389
Will Coe <williamcoewilkens@gmail.com>
2019-03-18 10:09:52 -0700

    authentication

--- script.js
+++ script.js
@@ -4,17 +4,20 @@
 let searchName;
 let searchCity;
 
+
+
 // loads modal on page load
 $(window).on('load', function () {
     $('#exampleModalCenter').modal('show')
 });
 
+// load modal on button click 
+$("#modal-button").on('click', function(event) {
+    $("#login-modal").modal('show')
+});
 
 // functiomn run after DOM loads
 
-// global variables to be set when user selects values in modal
-let searchName;
-let searchCity;
 
 
 $(document).ready(function () {
@@ -43,6 +46,102 @@ $(document).ready(function () {
         }
     });
 });
+
+// function for login info submit button
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAtSkxYOaVlS4XMUa2Ja2zsU-Q9Z2hbEg0",
+    authDomain: "project-1-7fdb3.firebaseapp.com",
+    databaseURL: "https://project-1-7fdb3.firebaseio.com",
+    projectId: "project-1-7fdb3",
+    storageBucket: "project-1-7fdb3.appspot.com",
+    messagingSenderId: "926155997785"
+  };
+  firebase.initializeApp(config);
+
+// sign up button function
+$("#create-button").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabs user input
+    var signUpEmail = $("#userEmail").val().trim();
+    var signUpPassword = $("#userPassword").val().trim();
+
+    // Creates local obj for train data
+    // var loginObject = {
+    //     email: emailLogin,
+    //     password: passwordLogin,
+    // };
+ // var loginObject = {
+    //     email: emailLogin,
+    //     password: passwordLogin,
+    // };
+    //   use Firebase function to add userEmail/password combo
+
+        firebase.auth().createUserWithEmailAndPassword(signUpEmail, signUpPassword)
+        // catch(function(error) {
+        //     // Handle Errors here.
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     // ...
+
+
+        $("#userEmail").val("");
+        $("#userPassword").val("");
+
+         // checking for if user logged into firebase 
+    var user = firebase.auth().currentUser;
+    if (user){
+        alert ("you're logged in")
+    }
+    else 
+        alert ("login failed")
+
+        // TODO add logic to only hide modal after succesful login/sign up
+        $("#login-modal").modal("hide");
+
+
+           });
+
+
+           // Sign in button function
+$("#login-button").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabs user input
+    var loginEmail = $("#userEmail").val().trim();
+    var loginPassword = $("#userPassword").val().trim();
+
+    firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+    
+
+    // // Clears it out
+    $("#userEmail").val("");
+    $("#userPassword").val("");
+
+    // TODO set up with promise and then display message in navbar if user is logged in
+    // checking for if user logged into firebase 
+    var user = firebase.auth().currentUser;
+     if (user){
+        alert ("you're logged in")
+    }
+    else 
+        alert ("login failed")
+
+
+    // TODO add logic to only hide modal after succesful login/sign up
+    $("#login-modal").modal("hide");
+});
+
+$("#logout-button").on("click", function (event) {
+    event.preventDefault();
+
+    //   use Firebase function to add userEmail/password combo
+
+        firebase.auth().signOut();
+});
+
 
 // function to call Edamam API, call is on eatin.html
 function displayRecipes() {
